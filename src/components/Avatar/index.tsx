b39ba7a628ac87ccb9dfe0f6d9a545a474396247
Piotr Łepkowski <ozrabal@gmail.com>
2025-04-14 22:16:46 +0200

    feat: Implement  components page with base components

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,23 @@
+import { Avatar as AvatarRoot, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import type { ComponentProps } from "react";
+
+type AvatarProps = React.ComponentProps<typeof AvatarRoot> & {
+  image: string;
+  alt: string;
+  fallback: string;
+};
+
+export default function Avatar({ image, alt, fallback, ...props }: AvatarProps) {
+  return (
+    <AvatarRoot {...props}>
+      <AvatarImage className="w-full h-full rounded-full object-cover" src={image} alt={alt} />
+      <AvatarFallback
+        className="flex items-center justify-center w-full h-full bg-white text-[15px] font-medium text-violet-500"
+        delayMs={600}
+      >
+        {fallback}
+        <span className="sr-only">{alt}</span>
+      </AvatarFallback>
+    </AvatarRoot>
+  );
+}
--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 
 function Avatar({ className, ...props }: React.ComponentProps<typeof AvatarPrimitive.Root>) {
   return (
-    <AvatarPrimitive.Root
+    <AvatarPrimitive.Root 
       data-slot="avatar"
       className={cn("relative flex size-8 shrink-0 overflow-hidden rounded-full", className)}
       {...props}
