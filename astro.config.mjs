bf7e78adff372ffc9b027d40a8b2705ee992634a
Piotr Łepkowski <ozrabal@gmail.com>
2025-05-12 22:44:35 +0200

    chore: update dependencies and add wrangler for Cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-// @ts-check
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,8 +15,16 @@ export default defineConfig({
   server: { port: 3000 },
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
   },
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
+  experimental: {
+    session: true, // Enable experimental sessions
+  },
 });
